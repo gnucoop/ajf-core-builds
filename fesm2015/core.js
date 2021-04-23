b7c71495ab867e1415250686a7073b04c7e64e77
Marco Marche <marco.marche@gnucoop.com>
2021-04-23 10:30:44 +0000

    master - 131f8fa5d release: bump version to 12.0.0-next.2 w/ changelog

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -22,7 +22,7 @@ import { Version } from '@angular/core';
  *
  */
 /** Current version of Ajf. */
-const VERSION = new Version('12.0.0-next.1-sha-3e6d1184a');
+const VERSION = new Version('12.0.0-next.2-sha-131f8fa5d');
 
 /**
  * @license
