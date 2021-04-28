0be1a01381b5428fca4d7c389ff12b724bcd955e
sara <sara.repossi@gmail.com>
2021-04-28 15:41:31 +0000

    master - b72ccd11e build: bump gic from 5.6.0 to 5.6.1

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -22,7 +22,7 @@ import { Version } from '@angular/core';
  *
  */
 /** Current version of Ajf. */
-const VERSION = new Version('12.0.0-next.2-sha-d7f9b1527');
+const VERSION = new Version('12.0.0-next.2-sha-b72ccd11e');
 
 /**
  * @license
