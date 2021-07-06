f8c8a701a481a71d9808b7b46e800bac2a8f1c0e
peppedeka <peppedeka@gmail.com>
2021-07-06 13:52:51 +0000

    master - c0898d0 build: make peppedeka owner

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -22,7 +22,7 @@ import { Version } from '@angular/core';
  *
  */
 /** Current version of Ajf. */
-const VERSION = new Version('12.1.0-next.1-sha-c6de8a6');
+const VERSION = new Version('12.1.0-next.1-sha-c0898d0');
 
 /**
  * @license
