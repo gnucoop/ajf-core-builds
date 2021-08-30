1d2df12cf8de6bc84baa44cb7999125b3b52da45
Marco Marche <marco.marche@gnucoop.com>
2021-08-30 08:46:54 +0000

    master - f8d4189 release: bump version to 12.1.0-next.2 w/ changelog

--- bundles/core.umd.js
+++ bundles/core.umd.js
@@ -26,7 +26,7 @@
 	 *
 	 */
 	/** Current version of Ajf. */
-	var VERSION = new core.Version('12.1.0-next.1-sha-c8484d1');
+	var VERSION = new core.Version('12.1.0-next.2-sha-f8d4189');
 
 	/**
 	 * @license
